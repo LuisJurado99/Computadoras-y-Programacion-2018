ac3ac560f7605a3dd4378fb78d453907bf270edb
LuisJurado99 <31529329+LuisJurado99@users.noreply.github.com>
2017-10-18 14:19:16 -0500

    Add files via upload

--- Suma Resta.cpp
+++ Suma Resta.cpp
@@ -10,11 +10,12 @@ int main(){
 	int su=0;
 	int mat=0;	
 	int i=0;
-	char op;
-printf ("Este programa te puede restar y sumar\n Elije que quieres hacer \"+\" ó \"-\" \n Presiona enter para salir\n");	
-scanf ("%c", &op); //inicio de case
-switch (op) {  //inicio del switch 
-case '+': //inicio de case en suma
+	int  op;
+do{
+printf ("Este programa te puede restar y sumar\n Elije que quieres hacer: \n1.- Suma.\n2.-Resta \n3.-Salir\n");	
+scanf ("%i", &op);//inicio de case
+switch (op) {  //inicio del switch
+case 1: //inicio de case en suma
 printf ("Número de elementos que deseas sumar. \n");
 scanf ("%d", &mat);
 for (i=1; i<=mat; i++){
@@ -24,7 +25,7 @@ for (i=1; i<=mat; i++){
 }
 printf ("El total de la suma es: %d \n", su);
 break; //fin de la suma 
-case '-': //inicio de case en resta 
+case 2: //inicio de case en resta 
 resta=0;
 printf("Introduce los elementos a restar: ");
   scanf ("%d", &tam);
@@ -36,10 +37,11 @@ resta=v[0];
 for(i=1;i<tam;i++){
     resta = resta-v[i];
 }
+printf ("El resultado de la resta es: %d\n", resta);
  /* Fin del case and switch
 Todo bien */
-}
- 
+} 
+} while (op != 3);
 printf ("Fin del programa");
 return 0;
 }
