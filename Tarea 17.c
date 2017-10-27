130cbf10611d88c9e7489a0df8ad191f931681fd
LuisJurado99 <31529329+LuisJurado99@users.noreply.github.com>
2017-10-27 11:43:02 -0500

    Add files via upload

---
+++ Tarea 17.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    int var1=5;
+    int var2=2;
+    int resultado=0;
+    
+   // Igual a ?
+    resultado = var1 == var2;
+    printf ("%d == %d=%d\n", var1, var2, resultado);
+    
+   // Menor igual que ?
+    resultado= var1 <= var2;
+    printf ("%d <= %d=%d \n", var1, var2, resultado);
+    
+   // mayor que ?
+    resultado= var1 > var2;
+    printf("%d > %d = %d\n", var1, var2, resultado);
+    
+   // diferente de ?
+    resultado= var1 != var2;
+    printf ("%d != %d = %d \n", var1, var2, resultado);
+    
+   // uso de &&
+    resultado= (var1 > var2) && (var1 != var2);
+    printf ("(%d > %d) && (%d != %d) = %d\n", var1, var2, var1, var2, resultado);
+    return (EXIT_SUCCESS);
+}
+
