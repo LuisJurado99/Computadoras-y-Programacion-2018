7638bbe09e19d9a640b8cb41ddede31ef12216d4
LuisJurado99 <31529329+LuisJurado99@users.noreply.github.com>
2017-11-06 23:33:28 -0600

    Update Tarea 19.c

--- Tarea 19.c
+++ Tarea 19.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int fncuadro (int a){
+int fncuadro (int a, int b){
     int j=0;
     int k=0;
     
     for (k=1; k<=a; k++){
         printf ("\n");
-        for (j=1; j<=a; j++){
+        for (j=1; j<=b; j++){
         printf ("+");    
         }
     }
@@ -15,10 +15,39 @@ int fncuadro (int a){
 }
 int main(int argc, char** argv) {
     int a=0;
-    printf ("¿De que tamaño deseas el cuadro?");
+    int b=0;
+    printf ("¿De que tamaño deseas el largo?");
     scanf ("%d", &a);
-    fncuadro(a);
+    printf ("¿De que tamaño deseas el ancho");
+    scanf ("%d", &b);
+    fncuadro(a, b);
     
     return (EXIT_SUCCESS);
 }
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int fncuadro (int a, int b){
+    int j=0;
+    int k=0;
+    
+    for (k=1; k<=a; k++){
+        printf ("\n");
+        for (j=1; j<=b; j++){
+        printf ("+");    
+        }
+    }
+    return 0;
+}
+int main(int argc, char** argv) {
+    int a=0;
+    int b=0;
+    printf ("¿De que tamaño deseas el largo?");
+    scanf ("%d", &a);
+    printf ("¿De que tamaño deseas el ancho");
+    scanf ("%d", &b);
+    fncuadro(a, b);
+    
+    return (EXIT_SUCCESS);
+}
