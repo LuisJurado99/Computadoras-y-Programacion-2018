bc0df1d59128b5c5d2f77fbe7d523d4338781528
LuisJurado99 <31529329+LuisJurado99@users.noreply.github.com>
2017-11-08 00:01:24 -0600

    Add files via upload

---
+++ Funciones/funcion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "funciones1.h"
+
+int main () {
+	float precio=0.0f;
+	printf ("¿Cuál es el precio del producto?");
+	scanf ("%f", &precio);
+	printf ("Valor del precio Total: %.2f\n", precio);
+	printf ("Valor del IVA: %.2f\n", fniva(precio));
+	printf ("Valor sin IVA: %.2f\n", fnsiniva(precio));
+	
+	return (0);
+}
