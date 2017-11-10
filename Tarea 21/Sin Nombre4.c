5108dd504a2d8c388494df23d9240f6b8bcd1b69
LuisJurado99 <31529329+LuisJurado99@users.noreply.github.com>
2017-11-09 21:14:39 -0600

    Add files via upload

---
+++ Tarea 21/Sin Nombre4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "cabecera.h"
+
+int main (){
+	
+	fnL ();
+	printf("\n");
+	
+	fnU ();
+	printf("\n");
+	
+	fnI ();
+	printf("\n");
+	fnS ();
+	return 0;
+}
---
+++ Tarea 21/cabecera.h
@@ -0,0 +1,33 @@
+void fnL (){
+	printf ("+\n+\n+\n+\n+\n");
+	printf ("+++++\n");
+}
+void fnU (){
+	printf("+   +\n");
+	printf("+   +\n");
+	printf("+   +\n");
+	printf("+   +\n");
+	printf("+++++\n");
+}
+void fnI (){
+	printf("+++++\n");
+	printf("  +  \n");
+	printf("  +  \n");
+	printf("  +  \n");
+	printf("+++++\n");
+}
+void fnS (){
+	printf("+++++\n");
+	printf("+\n");
+	printf("+++++\n");
+	printf("    +\n");
+	printf("+++++\n");
+}
+void fnA (){
+	printf("+++++\n");
+	printf("+   +\n");
+	printf("+++++\n");
+	printf("+   +\n");
+	printf("+   +\n");
+}
+
